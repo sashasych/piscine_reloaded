7c129ad58994ceafa8346d19612a2e91afa93ecf
Microdosing Harissa <mharissa@oa-l1.21-school.ru>
2019-09-09 12:34:57 +0300

    first commit

---
+++ ex27/includes/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mharissa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/20 16:44:55 by mharissa          #+#    #+#             */
+/*   Updated: 2019/06/20 18:43:48 by mharissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <sys/uio.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+#endif
